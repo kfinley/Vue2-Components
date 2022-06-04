deb9e8d3816c12e2309a0848883b4ac7bc903de0
Kyle Finley <kyle@kylefinley.net>
2022-06-04 15:11:05 -0700

    update: added components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,9 @@ import BubbleButton from './buttons/bubble-button.vue';
 import BubbleButtonBanner from './buttons/bubble-button-banner.vue';
 import Button from './buttons/button.vue';
 import Card from './card.vue';
+import Entity from './entity.vue';
+import Draggable from './draggable.vue';
+import DraggableEntity from './draggable-entity.vue';
 import FatToggle from './toggles/fat-toggle.vue';
 import FlatModal from './flat-modal.vue';
 import FlatToggle from './toggles/flat-toggle.vue';
@@ -18,6 +21,9 @@ export {
   BubbleButtonBanner,
   Button,
   Card,
+  Draggable,
+  DraggableEntity,
+  Entity,
   FatToggle,
   FlatModal,
   FlatToggle,
