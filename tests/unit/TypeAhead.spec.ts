2ca8c0e1264e0b8afd31095c316590a2295ec0e3
Kyle Finley <kyle@kylefinley.net>
2022-06-15 07:33:53 -0700

    update: Added focus and result list tests for Type Ahead

--- tests/unit/TypeAhead.spec.ts
+++ tests/unit/TypeAhead.spec.ts
@@ -22,6 +22,18 @@ describe("type-ahead.vue", () => {
     expect(component.isVueInstance).toBeTruthy();
   });
 
+  it("Fires focus on mount", () => {
+
+    // Arrange
+
+    const setValuePromise = component.find('input');
+
+    // Act (the act is mounting in this case)
+    // Assert
+    expect(component.emitted('focus')?.length == 1).toBeTruthy();
+
+  });
+
   it("Fires update on 3 characters input", () => {
 
     // Arrange
@@ -51,7 +63,25 @@ describe("type-ahead.vue", () => {
       .then(() => { // Assert
         const updateEvent = component.emitted('update');
         expect(updateEvent).toBe(undefined);
-      })
+      });
   });
 
+  it("Should show a list of results after at least 3 characters are input", () => {
+
+    // Arrange
+
+    const setPromise = component.find('input').setValue('New') as Promise<void>;
+
+    setPromise
+      .then(() => jest.runAllTimers()) // Act - Wait for time to pass...
+      .then(() => { // Assert
+
+        const typeAheadContainer = component.find('.typeahead-dropdown-container');
+        const resultsList = typeAheadContainer.find('ul'); // First ul in the container is the result list. Second ul is the No Results ul.
+
+        expect(resultsList).toBeTruthy();
+        expect(resultsList.findAll('li').length).toBe(4); // Should contain the 4 states with 'New' in the name
+      });
+
+  });
 })
