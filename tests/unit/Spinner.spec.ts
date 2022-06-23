7423c66ef58dd5825b6ad7c8342511c856c71803
Kyle Finley <kyle@kylefinley.net>
2022-06-23 08:04:18 -0700

    update: Added Spinner specs

---
+++ tests/unit/Spinner.spec.ts
@@ -0,0 +1,20 @@
+import Components from "@/components";
+import { Wrapper } from "@vue/test-utils";
+import { Factory } from "../utils";
+
+describe("spinner.vue", () => {
+
+  let component: Wrapper<Vue, Element>;
+
+  beforeEach(() => {
+    // Arrange
+    component = Factory.create(Components.Spinner);
+
+  });
+
+  it("mounts", () => {
+
+    // Assert
+    expect(component.isVueInstance).toBeTruthy();
+  });
+});
