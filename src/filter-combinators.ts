97c096051d94a3de1f54c3907b982d8ed44af052
Kyle Finley <kyle@kylefinley.net>
2023-03-24 22:11:52 +0000

    update: added filter-combinators

---
+++ src/filter-combinators.ts
@@ -0,0 +1,57 @@
+//ht: https://codepen.io/Universalist/post/predicates-in-javascript
+export const and = (p1, p2) => {
+  return function (x) {
+    // console.log('and', p1(x) && p2(x));
+    return p1(x) && p2(x);
+  };
+};
+
+export const not = (p) => {
+  return function (x) {
+    // console.log('not', !p(x));
+    return !p(x);
+  };
+};
+
+export const equals = (prop, value) => {
+  return function (x: any) {
+    // console.log('equals', x[prop] === value);
+
+    // console.log('prop', prop);
+    // console.log(value);
+    // console.log(x[prop]);
+    return x[prop] == value;
+  };
+};
+
+export const less = (x) => {
+  return function (y) {
+    return y < x;
+  };
+};
+
+export const greater = (x) => {
+  return function (y) {
+    return y > x;
+  };
+};
+
+export const createEqualsPredicate = function createEqualsPredicate<T>(
+  props: Record<string, any>
+) {
+  const equalsConditions: ((value: T, index: number, obj: T[]) => unknown)[] = [];
+
+  for (const p in props) {
+    equalsConditions.push(equals(p, props[p]));
+  }
+
+  let condition: (value: T, index: number, obj: T[]) => unknown = equalsConditions[0];
+
+  equalsConditions.forEach((e) => {
+    if (e !== condition) {
+      condition = and(condition, e);
+    }
+  });
+
+  return condition;
+};
